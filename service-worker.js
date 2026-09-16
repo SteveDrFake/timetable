23f69debd1b11bd10e058f518f3378d5b7343074
Steve_Dr.Fake <323191399+SteveDrFake@users.noreply.github.com>
2026-09-16 11:48:08 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,170 @@
-const CACHE = "tku-course-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json"
+const CACHE_NAME = "tku-timetable-v2";
+
+
+const FILES_TO_CACHE = [
+
+    "./",
+
+    "./index.html",
+
+    "./style.css",
+
+    "./app.js",
+
+    "./manifest.json"
+
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys
-        .filter(k => k !== CACHE)
-        .map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
+self.addEventListener(
+    "install",
 
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
+    event => {
 
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        return response;
-      }).catch(() => caches.match("./index.html"));
-    })
-  );
-});
+        event.waitUntil(
+
+            caches.open(
+                CACHE_NAME
+            )
+            .then(
+                cache =>
+                    cache.addAll(
+                        FILES_TO_CACHE
+                    )
+            )
+
+        );
+
+
+        self.skipWaiting();
+
+    }
+
+);
+
+
+self.addEventListener(
+    "activate",
+
+    event => {
+
+        event.waitUntil(
+
+            caches.keys()
+
+                .then(
+                    keys =>
+
+                        Promise.all(
+
+                            keys
+
+                                .filter(
+                                    key =>
+                                        key !==
+                                        CACHE_NAME
+                                )
+
+                                .map(
+                                    key =>
+                                        caches.delete(
+                                            key
+                                        )
+                                )
+
+                        )
+
+                )
+
+        );
+
+
+        self.clients.claim();
+
+    }
+
+);
+
+
+self.addEventListener(
+    "fetch",
+
+    event => {
+
+        if (
+            event.request.method !==
+            "GET"
+        ) {
+
+            return;
+
+        }
+
+
+        event.respondWith(
+
+            caches.match(
+                event.request
+            )
+
+            .then(
+                cachedResponse => {
+
+                    if (
+                        cachedResponse
+                    ) {
+
+                        return cachedResponse;
+
+                    }
+
+
+                    return fetch(
+                        event.request
+                    )
+
+                    .then(
+                        response => {
+
+                            const copy =
+                                response.clone();
+
+
+                            caches.open(
+                                CACHE_NAME
+                            )
+                            .then(
+                                cache => {
+
+                                    cache.put(
+                                        event.request,
+                                        copy
+                                    );
+
+                                }
+                            );
+
+
+                            return response;
+
+                        }
+                    )
+
+                    .catch(
+                        () =>
+
+                            caches.match(
+                                "./index.html"
+                            )
+
+                    );
+
+                }
+
+            )
+
+        );
+
+    }
+
+);

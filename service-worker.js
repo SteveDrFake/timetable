4ce7103b0a8db79ad944f9b722149671443b5768
Steve_Dr.Fake <323191399+SteveDrFake@users.noreply.github.com>
2026-09-16 14:16:56 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,170 +1,26 @@
-const CACHE_NAME = "tku-timetable-v2";
-
-
-const FILES_TO_CACHE = [
-
-    "./",
-
-    "./index.html",
-
-    "./style.css",
-
-    "./app.js",
-
-    "./manifest.json"
-
-];
-
-
-self.addEventListener(
-    "install",
-
-    event => {
-
-        event.waitUntil(
-
-            caches.open(
-                CACHE_NAME
-            )
-            .then(
-                cache =>
-                    cache.addAll(
-                        FILES_TO_CACHE
-                    )
-            )
-
-        );
-
-
-        self.skipWaiting();
-
-    }
-
-);
-
-
-self.addEventListener(
-    "activate",
-
-    event => {
-
-        event.waitUntil(
-
-            caches.keys()
-
-                .then(
-                    keys =>
-
-                        Promise.all(
-
-                            keys
-
-                                .filter(
-                                    key =>
-                                        key !==
-                                        CACHE_NAME
-                                )
-
-                                .map(
-                                    key =>
-                                        caches.delete(
-                                            key
-                                        )
-                                )
-
-                        )
-
-                )
-
-        );
-
-
-        self.clients.claim();
-
-    }
-
-);
-
-
-self.addEventListener(
-    "fetch",
-
-    event => {
-
-        if (
-            event.request.method !==
-            "GET"
-        ) {
-
-            return;
-
-        }
-
-
-        event.respondWith(
-
-            caches.match(
-                event.request
-            )
-
-            .then(
-                cachedResponse => {
-
-                    if (
-                        cachedResponse
-                    ) {
-
-                        return cachedResponse;
-
-                    }
-
-
-                    return fetch(
-                        event.request
-                    )
-
-                    .then(
-                        response => {
-
-                            const copy =
-                                response.clone();
-
-
-                            caches.open(
-                                CACHE_NAME
-                            )
-                            .then(
-                                cache => {
-
-                                    cache.put(
-                                        event.request,
-                                        copy
-                                    );
-
-                                }
-                            );
-
-
-                            return response;
-
-                        }
-                    )
-
-                    .catch(
-                        () =>
-
-                            caches.match(
-                                "./index.html"
-                            )
-
-                    );
-
-                }
-
-            )
-
-        );
-
-    }
-
-);
+const CACHE="tku-timetable-v3";
+const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json"];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
+  event.respondWith(
+    caches.match(event.request).then(cached=>{
+      if(cached)return cached;
+      return fetch(event.request).then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(c=>c.put(event.request,copy));
+        return response;
+      }).catch(()=>caches.match("./index.html"));
+    })
+  );
+});
